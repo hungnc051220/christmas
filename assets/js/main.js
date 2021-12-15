5d5f476a33a9dc77cb4f49a43527519081d0ffd5
ADMIN-PC\Admin <hungnc051220@gmail.com>
2021-12-15 14:36:17 +0700

    Header

---
+++ assets/js/main.js
@@ -0,0 +1,29 @@
+/*=============== SHOW MENU ===============*/
+const navMenu = document.getElementById("nav-menu"),
+  navToggle = document.getElementById("nav-toggle"),
+  navClose = document.getElementById("nav-close");
+
+  if(navToggle){
+      navToggle.addEventListener('click', () => {
+        navMenu.classList.add('show-menu');
+      })
+  }
+
+  if(navClose) {
+      navClose.addEventListener('click', () => {
+          navMenu.classList.remove('show-menu');
+      })
+  }
+/*=============== REMOVE MENU MOBILE ===============*/
+
+/*=============== CHANGE BACKGROUND HEADER ===============*/
+
+/*=============== NEW SWIPER ===============*/
+
+/*=============== SCROLL SECTIONS ACTIVE LINK ===============*/
+
+/*=============== SHOW SCROLL UP ===============*/
+
+/*=============== DARK LIGHT THEME ===============*/
+
+/*=============== SCROLL REVEAL ANIMATION ===============*/
